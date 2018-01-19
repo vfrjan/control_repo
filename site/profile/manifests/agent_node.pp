4eedc325fe65169ac20db741508607e4ef991225
vfrjan <32195676+vfrjan@users.noreply.github.com>
2018-01-19 13:43:00 +0100

    Update agent_node.pp

--- site/profile/manifests/agent_node.pp
+++ site/profile/manifests/agent_node.pp
@@ -2,6 +2,7 @@ class profile::agent_node {
   include dockeragent
   dockeragent::node {'web.puppet.vm':}
   dockeragent::node {'db.puppet.vm':}
+  dockeragent::node {'minetest.puppet.vm':}
   host {'web.puppet.vm':
     ensure => present,
     ip     => '172.18.0.2',
