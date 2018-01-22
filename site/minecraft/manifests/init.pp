7cde52df1e92f62c8d5faa1c5414551f3f601e61
vfrjan <32195676+vfrjan@users.noreply.github.com>
2018-01-22 09:30:35 +0100

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -18,7 +18,9 @@ class minecraft (
   }
   file {'/etc/systemd/system/minecraft.service':
     ensure => file,
-    source => ‘puppet:///modules/minecraft/minecraft.service’,
+    content => epp('minecraft/minecraft.service',{
+      install_dir => $install_dir
+    })
   }
   service { ‘minecraft’:
     ensure => running,
