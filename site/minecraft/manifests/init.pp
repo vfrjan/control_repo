a5214cd3b2e2958f9ea81bb1f1c9d219a431f295
vfrjan <32195676+vfrjan@users.noreply.github.com>
2018-01-22 08:58:46 +0100

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -20,5 +20,6 @@ class minecraft {
   service { ‘minecraft’:
     ensure => running,
     enable => true,
+    require => [Package['java'],File['/opt/minecraft/eula.txt'],File['/etc/systemd/system/minecraft.service']]
   }
 }
